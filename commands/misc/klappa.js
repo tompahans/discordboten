2c6d10f1f95659a5ade80725b351c27f8baf09cd
Tommy Jämsä <hassemedh@gmail.com>
2023-05-06 12:21:25 +0200

    chore(klappa.js): add ester images

--- commands/misc/klappa.js
+++ commands/misc/klappa.js
@@ -13,56 +13,60 @@ const path = require("path");
 const pathToImages = "./data/images/";
 
 const getRandomInt = (min, max) => {
-	return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 const klappaKatt = (message, args) => {
-	const folder = pathToImages + args[0].toLowerCase() + "/";
-	if (fs.existsSync(folder)) {
-		fs.readdir(folder, function (err, files) {
-			if (err) {
-				console.log(err);
-				return;
-			}
-			const imageFiles = files.filter(function (file) {
-				return (
-					file.endsWith(".JPG") || file.endsWith("png") || file.endsWith("jpg")
-				);
-			});
+  const folder = pathToImages + args[0].toLowerCase() + "/";
+  if (fs.existsSync(folder)) {
+    fs.readdir(folder, function (err, files) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      const imageFiles = files.filter(function (file) {
+        return (
+          file.endsWith(".JPG") ||
+          file.endsWith(".png") ||
+          file.endsWith(".PNG") ||
+          file.endsWith(".jpg") ||
+          file.endsWith(".JPEG")
+        );
+      });
+      const random = getRandomInt(0, imageFiles.length - 1);
+      const image = imageFiles[random];
+      const getPath = path.resolve(__dirname, "..", "..", folder + image);
 
-			const image = imageFiles[getRandomInt(0, imageFiles.length - 1)];
-			const getPath = path.resolve(__dirname, "..", "..", folder + image);
+      const attachment = new AttachmentBuilder(getPath, {
+        name: image,
+      });
 
-			const attachment = new AttachmentBuilder(getPath, {
-				name: image,
-			});
-
-			return message.reply({ content: "Mjaau", files: [attachment] });
-		});
-	}
+      return message.reply({ content: "Mjaau", files: [attachment] });
+    });
+  }
 };
 
 /**
  * @type {import('../../typings').LegacyCommand}
  */
 module.exports = {
-	name: "klappa",
-	description: "Skriv !klappa <kattnamn>",
-	aliases: ["commands"],
-	usage: "[klappa tarzan]",
-	args: true,
-	cooldown: 5,
-	// Refer to typings.d.ts for available properties.
+  name: "klappa",
+  description: "Skriv !klappa <kattnamn>",
+  aliases: ["commands"],
+  usage: "[klappa tarzan]",
+  args: true,
+  cooldown: 5,
+  // Refer to typings.d.ts for available properties.
 
-	execute(message, args) {
-		if (!args.length) {
-			/**
-			 * @type {EmbedBuilder}
-			 * @description what type of pizza you rolled in an embed
-			 */
+  execute(message, args) {
+    if (!args.length) {
+      /**
+       * @type {EmbedBuilder}
+       * @description what type of pizza you rolled in an embed
+       */
 
-			return message.reply({ content: "Förstår inte frågan" });
-		}
-		klappaKatt(message, args);
-	},
+      return message.reply({ content: "Förstår inte frågan" });
+    }
+    klappaKatt(message, args);
+  },
 };
